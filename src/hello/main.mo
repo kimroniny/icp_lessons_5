aae04be4458b79318a1fc99572d259ac5f338181
kimroniny <1301862177@qq.com>
2022-03-20 22:34:15 +0800

    first

--- src/hello/main.mo
+++ src/hello/main.mo
@@ -1,5 +1,90 @@
+import List "mo:base/List";
+import Iter "mo:base/Iter";
+import Principal "mo:base/Principal";
+import Time "mo:base/Time";
+import Debug "mo:base/Debug";
+
 actor {
-    public func greet(name : Text) : async Text {
-        return "Hello, " # name # "!";
+    stable var name: Text = "kimroniny";
+    public type Message = {
+        author: Text;
+        text: Text; 
+        time: Time.Time
+        };
+    stable var followed: List.List<(Principal, Text)> = List.nil();
+    stable var messages: List.List<Message> = List.nil();
+
+    public type Microblog = actor {
+        set_name: shared (Text, Text) -> async ();
+        get_name: shared query () -> async ?Text;
+        follow: shared (Principal) -> async();
+        follows: shared query () -> async [Principal];
+        post: shared (Text, Text) -> async ();
+        posts: shared query (Time.Time) -> async [Message];
+        timeline: shared ([Principal], Time.Time) -> async [Message];
     };
-};
+
+    public shared func follow(id : Principal) : async () {
+        let canister : Microblog = actor(Principal.toText(id));
+        let author = switch (await canister.get_name()) {
+            case null "invalid author name!!!";
+            case (?Text) Text; 
+        };
+        followed := List.push((id, author), followed);
+    };
+
+    public shared query func follows() : async [(Principal, Text)] {
+        List.toArray(followed)
+    };
+
+    public shared (msg) func post(opt: Text, text: Text) : async (Time.Time) {
+        assert(opt == "kimroniny");
+        let now = Time.now();
+        let postMsg = {
+            author = name;
+            text = text; 
+            time = now;
+            };
+        messages := List.push(postMsg, messages);
+        now
+    };
+
+    public shared query func posts(since: Time.Time) : async [Message] {
+        let sinceList = List.filter(
+            messages, 
+            func(msg: Message) : Bool{
+                msg.time > since
+            }
+        );
+        List.toArray(sinceList)
+    };
+
+    public shared func timeline(author: [Principal], since: Time.Time) : async [Message] {
+        var all : List.List<Message> = List.nil();
+        var authorList : List.List<Principal> = List.nil();
+        let getId = func ((id: Principal, name: Text)) : Principal{
+            id
+        };
+        if (author.size() == 0){
+            authorList := List.map(followed, getId);
+        }else {
+            authorList := List.fromArray(author);
+        };
+        for (id in Iter.fromList(authorList)) {
+            let canister : Microblog = actor(Principal.toText(id));
+            let msgs = await canister.posts(since);
+            for (msg in Iter.fromArray(msgs)) {
+                all := List.push(msg, all)
+            }
+        };
+        List.toArray(all)
+    };
+    public shared func set_name(opt: Text, uname : Text) : async () {
+        assert(opt == "kimroniny");
+        name := uname;
+    };
+    public shared query func get_name() : async ?Text {
+        ?name
+    };
+
+}
--- src/hello_assets/src/index.js
+++ src/hello_assets/src/index.js
@@ -1,19 +1,124 @@
 import { hello } from "../../declarations/hello";
+import {Principal} from "@dfinity/principal";
 
-document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = e.target.querySelector("button");
+async function post() { 
+  let post_button = document.getElementById("post");
+  let error = document.getElementById("error");
+  error.innerText = "";
+  post_button.disable = true;
+  let textarea = document.getElementById("message");
+  let opt = document.getElementById("opt").value;
+  try{
+    await hello.post(opt, textarea.value);
+    textarea.value = "";
+  }catch(err){
+    error.innerText = "Post Failed!";
+  }
+  post_button.disable = false;
+  load_posts("0");
+}
 
-  const name = document.getElementById("name").value.toString();
+async function set_name() { 
+  let set_button = document.getElementById("btn_set");
+  let error = document.getElementById("set_error");
+  error.innerText = "";
+  set_button.disable = true;
+  let textarea = document.getElementById("set");
+  let opt = document.getElementById("opt").value;
+  try{
+    await hello.set_name(opt, textarea.value);
+  }catch(err){
+    error.innerText = "Set Failed!";
+  }
+  set_button.disable = false;
+  error.innerText = "Success!";
+}
 
-  button.setAttribute("disabled", true);
+async function follow() { 
+  let follow_button = document.getElementById("btn_follow");
+  let error = document.getElementById("follow_error");
+  error.innerText = "";
+  follow_button.disable = true;
+  let textarea = document.getElementById("follow");
+  try{
+    await hello.follow(Principal.fromText(textarea.value));
+  }catch(err){
+    error.innerText = "Set Failed!";
+  }
+  follow_button.disable = false;
+  reflesh_author_list();
+}
 
-  // Interact with foo actor, calling the greet method
-  const greeting = await hello.greet(name);
+async function reflesh_author_list(){
+  let followed = await hello.follows();
+  let followed_ul = document.getElementById("author_list");
+  followed_ul.replaceChildren([]);
+  let myself = document.createElement("input");
+  myself.type = "radio";
+  myself.name = "authorList";
+  myself.id = "myself";
+  myself.value = "0";
+  myself.checked = true;
+  followed_ul.appendChild(myself);
+  let lab_myself = document.createElement("label");
+  lab_myself.innerText = "My message";
+  lab_myself.setAttribute("for", "myself");
+  followed_ul.appendChild(lab_myself);
+  followed_ul.appendChild(document.createElement("br"));
+  let lab_all_author = document.createElement("label");
+  lab_all_author.innerText = "Following Authors:";
+  followed_ul.appendChild(lab_all_author);
+  followed_ul.appendChild(document.createElement("br"));
+  followed.forEach(function(item){
+    let author = document.createElement("input");
+    author.type = "radio";
+    author.name = "authorList";
+    author.id = "author";
+    author.value = item[0].toText();
+    followed_ul.appendChild(author);
+    let lab_author = document.createElement("label");
+    lab_author.innerText = item[1];
+    lab_author.setAttribute("for", "author");
+    followed_ul.appendChild(lab_author);
+    followed_ul.appendChild(document.createElement("br"));
+  });
+}
 
-  button.removeAttribute("disabled");
+async function load_posts(author) {
+  let posts_section = document.getElementById("posts");
+  var posts;
+  if (author == undefined) return;
+  switch(author) {
+    case "0":
+      posts = await hello.posts(1000);
+      break;
+    default:
+      posts = await hello.timeline([Principal.fromText(author)], 1000);
+  }
+  posts_section.replaceChildren([]);
+  for (var i = 0; i< posts.length; i++) { 
+    let post = document.createElement("p");
+    post.innerText = "[" + new Date(Number(posts[i].time/1000000n)).toLocaleString() + "] <" + posts[i].author + "> " + posts[i].text;
+    posts_section.appendChild(post)
+  }
+}
 
-  document.getElementById("greeting").innerText = greeting;
+function load() { 
+  let post_button = document.getElementById("post");
+  post_button.onclick = post;
+  let set_button = document.getElementById("btn_set");
+  set_button.onclick = set_name;
+  let follow_button = document.getElementById("btn_follow");
+  follow_button.onclick = follow;
 
-  return false;
-});
+  let followed_ul = document.getElementById("author_list");
+  followed_ul.onclick = function (event) {
+    const {target} = event;
+    if (target.tagName.toLowerCase() !== "input") return;
+    load_posts(target.value);
+  };
+  load_posts();
+  reflesh_author_list();
+}
+
+window.onload = load;
